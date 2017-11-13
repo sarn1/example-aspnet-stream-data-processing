af8f0665341c0ccd1c59bbbc2f89019fb9b1b00b
sarn <sarn@tyndale.com>
2017-11-13 16:06:35 -0600

    stream readers

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ C# Streams and Data Processing
 
 https://teamtreehouse.com/library/c-streams-and-data-processing
  
-- 
+- Create new text file by adding to solution, but then pull it out of "Solution Files" into root.  Then you can right click and go to property to set `Copy to Output Directory` to `copy if newer`
+- UTF-8 for text, UTF-16 when creating within .NET.
+
---
+++ SoccerStats/SoccerStats/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; // new!
+
+namespace SoccerStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // print all text files in the directory
+            string currentDirectory = Directory.GetCurrentDirectory();
+            DirectoryInfo directory = new DirectoryInfo(currentDirectory);
+            var files = directory.GetFiles("*.txt");
+
+            foreach(var f in files)
+            {
+                System.Console.WriteLine(f.Name);
+            }
+
+            System.Console.ReadLine();
+
+            // read from file specified
+            string currentDirectory2 = Directory.GetCurrentDirectory();
+            DirectoryInfo directory2 = new DirectoryInfo(currentDirectory2);
+            var fileName = Path.Combine(directory2.FullName, "data.txt");
+            var file = new FileInfo(fileName);
+
+            if (file.Exists)
+            {
+                using (var reader = new StreamReader(fileName))
+                {
+                    Console.SetIn(reader);
+                }
+            }
+
+            //csv
+            string currentDirectory3 = Directory.GetCurrentDirectory();
+            DirectoryInfo directory3 = new DirectoryInfo(currentDirectory3);
+            string fileName2 = Path.Combine(directory3.FullName, "SoccerGameResults.csv");
+            var fileContents = ReadFile(fileName2);
+
+            //create line breaks in output
+            string[] fileLines = fileContents.Split(new char[] { '\r','\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var line in fileLines) {
+                Console.WriteLine(line);
+
+            }
+
+            Console.WriteLine("Press any key to close");
+            Console.ReadKey();
+        }
+
+        public static string ReadFile(string fileName)
+        {
+            using (var reader = new StreamReader(fileName))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
